60415b9e20651213f6ad4660af0f08c4041c87eb
RomeroFederico <romerof14@gmail.com>
2022-04-09 10:25:08 -0300

    modificacion por identacion...

--- busquedaBinariaRecursion.js
+++ busquedaBinariaRecursion.js
@@ -1,6 +1,6 @@
 function busquedaBinaria(array, search) {
-	if (array.length === 0 || !search)
-  	return -1;
+  if (array.length === 0 || !search)
+    return -1;
     
   var pivote = Math.floor(array.length / 2);
   var resultado;
@@ -8,16 +8,16 @@ function busquedaBinaria(array, search) {
   if (array[pivote] === search) return pivote;
   
   if (array[pivote] > search) {
-  	resultado = busquedaBinaria(array.slice(0, pivote), search);
+    resultado = busquedaBinaria(array.slice(0, pivote), search);
     if (resultado !== -1)
-    	return resultado;
+      return resultado;
   }
   else {
-  	resultado = busquedaBinaria(array.slice(pivote + 1), search);
+    resultado = busquedaBinaria(array.slice(pivote + 1), search);
     if (resultado !== -1)
-    	return resultado + pivote + 1;
+      return resultado + pivote + 1;
   }
- 	return -1;
+  return -1;
 }
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
